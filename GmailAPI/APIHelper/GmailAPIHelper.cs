6a0840232abba809885bf7f10726eae683ea5818
igorflyunt <igorflyunt2@gmail.com>
2021-08-14 21:24:00 +0300

    Add project files.

---
+++ GmailAPI/APIHelper/GmailAPIHelper.cs
@@ -0,0 +1,187 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Gmail.v1;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using Google.Apis.Gmail.v1.Data;
+
+namespace GmailAPI.APIHelper
+{
+    public static class GmailAPIHelper
+    {
+        static string[] Scores = { GmailService.Scope.MailGoogleCom };
+        static string ApplicationName = "Gmail API Application";
+
+        public static GmailService GetService()
+        {
+            UserCredential credential;
+            using (FileStream stream = new FileStream(Convert.ToString(ConfigurationManager.AppSettings["ClientInfo"]),
+                FileMode.Open, FileAccess.Read))
+            {
+                string FolderPath = Convert.ToString(ConfigurationManager.AppSettings["CredentialsInfo"]);
+                string FilePath = Path.Combine(FolderPath, "APITokenCredentials");
+
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scores,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(FilePath, true)).Result;
+            }
+
+            //Create Gmail API service
+            GmailService service = new GmailService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName
+            });
+            return service;
+        }
+
+        public static string MsgNestedParts(IList<MessagePart> payloadParts)
+        {
+            string str = string.Empty;
+            if (payloadParts.Count() < 0)
+            {
+                return string.Empty;
+            }
+            else
+            {
+                IList<MessagePart> plainTestMail = payloadParts.Where(parts => parts.MimeType == "text/plain").ToList();
+                IList<MessagePart> attachmentMail = payloadParts.Where(parts => parts.MimeType == "multipart/alternative").ToList();
+
+                if (plainTestMail.Count() > 0)
+                {
+                    foreach (MessagePart eachPart in plainTestMail)
+                    {
+                        if (eachPart.Parts == null)
+                        {
+                            if (eachPart.Body != null && eachPart.Body.Data != null)
+                            {
+                                str += eachPart.Body.Data;
+                            }
+                        }
+                        else
+                        {
+                            return MsgNestedParts(eachPart.Parts);
+                        }
+                    }
+                }
+
+                if (attachmentMail.Count() > 0)
+                {
+                    foreach (MessagePart eachPart in attachmentMail)
+                    {
+                        if (eachPart.Parts == null)
+                        {
+                            if (eachPart.Body != null && eachPart.Body.Data != null)
+                            {
+                                str += eachPart.Body.Data;
+                            }
+                        }
+                        else
+                        {
+                            return MsgNestedParts(eachPart.Parts);
+                        }
+                    }
+                }
+                return str;
+            }
+        }
+
+        public static List<string> GetAttachments(string userId, string messageId, string outputDir)
+        {
+            try
+            {
+                List<string> FileName = new List<string>();
+                GmailService gmailService = GetService();
+                Message message = gmailService.Users.Messages.Get(userId, messageId).Execute();
+                IList<MessagePart> messageParts = message.Payload.Parts;
+
+                foreach (var messagePart in messageParts)
+                {
+                    if (!String.IsNullOrEmpty(messagePart.Filename))
+                    {
+                        string attachId = messagePart.Body.AttachmentId;
+                        MessagePartBody attachPart =
+                            gmailService.Users.Messages.Attachments.Get(userId, messageId, attachId).Execute();
+                        byte[] data = Base64ToByte(attachPart.Data);
+                        File.WriteAllBytes(Path.Combine(outputDir, messagePart.Filename), data);
+                    }
+                }
+
+                return FileName;
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine("An error occurred: " + exception.Message);
+                return null;
+            }
+        }
+
+
+        public static string Base64Decode(string Base64Test)
+        {
+            string EncodTxt = string.Empty;
+            //STEP-1: Replace all special Character of Base64Test
+            EncodTxt = Base64Test.Replace("-", "+");
+            EncodTxt = EncodTxt.Replace("_", "/");
+            EncodTxt = EncodTxt.Replace(" ", "+");
+            EncodTxt = EncodTxt.Replace("=", "+");
+
+            //STEP-2: Fixed invalid length of Base64Test
+            if (EncodTxt.Length % 4 > 0) { EncodTxt += new string('=', 4 - EncodTxt.Length % 4); }
+            else if (EncodTxt.Length % 4 == 0)
+            {
+                EncodTxt = EncodTxt.Substring(0, EncodTxt.Length - 1);
+                if (EncodTxt.Length % 4 > 0) { EncodTxt += new string('+', 4 - EncodTxt.Length % 4); }
+            }
+
+            //STEP-3: Convert to Byte array
+            byte[] ByteArray = Convert.FromBase64String(EncodTxt);
+
+            //STEP-4: Encoding to UTF8 Format
+            return Encoding.UTF8.GetString(ByteArray);
+        }
+
+        public static byte[] Base64ToByte(string Base64Test)
+        {
+            string EncodTxt = string.Empty;
+            //STEP-1: Replace all special Character of Base64Test
+            EncodTxt = Base64Test.Replace("-", "+");
+            EncodTxt = EncodTxt.Replace("_", "/");
+            EncodTxt = EncodTxt.Replace(" ", "+");
+            EncodTxt = EncodTxt.Replace("=", "+");
+
+            //STEP-2: Fixed invalid length of Base64Test
+            if (EncodTxt.Length % 4 > 0) { EncodTxt += new string('=', 4 - EncodTxt.Length % 4); }
+            else if (EncodTxt.Length % 4 == 0)
+            {
+                EncodTxt = EncodTxt.Substring(0, EncodTxt.Length - 1);
+                if (EncodTxt.Length % 4 > 0) { EncodTxt += new string('+', 4 - EncodTxt.Length % 4); }
+            }
+
+            //STEP-3: Convert to Byte array
+            return Convert.FromBase64String(EncodTxt);
+        }
+
+     
+
+        public static void MsgMarkAsRead(string HostEmailAdress, string MsgId)
+        {
+            //Message marks as read after reading message
+            ModifyMessageRequest modifyMessageRequest = new ModifyMessageRequest();
+            modifyMessageRequest.AddLabelIds = null;
+            modifyMessageRequest.RemoveLabelIds = new List<string> { "UNREAD" };
+            GetService().Users.Messages.Modify(modifyMessageRequest, HostEmailAdress, MsgId).Execute();
+        }
+
+        
+    }
+}
---
+++ GmailAPI/Program.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Configuration;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Channels;
+using GmailAPI.APIHelper;
+using Google.Apis.Gmail.v1;
+using Google.Apis.Gmail.v1.Data;
+
+
+namespace GmailAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                List<Gmail> MailList = GetAllEmails(Convert.ToString(
+                    ConfigurationManager.AppSettings["HostAddress"]));
+                ;
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine("Error: " + exception);
+            }
+        }
+
+        public static List<Gmail> GetAllEmails(string HostEmailAddress)
+        {
+            try
+            {
+                GmailService gmailService = GmailAPIHelper.GetService();
+                List<Gmail> emailList = new List<Gmail>();
+                UsersResource.MessagesResource.ListRequest listRequest =
+                    gmailService.Users.Messages.List(HostEmailAddress);
+                listRequest.LabelIds = "INBOX";
+                listRequest.IncludeSpamTrash = false;
+                listRequest.Q = "is:unread"; //only for unread e-mails
+
+                //Get All Emails
+                ListMessagesResponse listMessagesResponse = listRequest.Execute();
+
+                if (listMessagesResponse != null && listMessagesResponse.Messages != null)
+                {
+                    //Loop through each email and get what fields I want
+                    foreach (Message msg in listMessagesResponse.Messages)
+                    {
+                        //Message marks as read after reading message
+                        GmailAPIHelper.MsgMarkAsRead(HostEmailAddress, msg.Id);
+
+                        UsersResource.MessagesResource.GetRequest Message =
+                            gmailService.Users.Messages.Get(HostEmailAddress, msg.Id);
+                        Console.WriteLine("\n --New Mail -- ");
+                        Console.WriteLine("Step-1: Message ID: " + msg.Id);
+
+                        //Make another request for that email id
+                        Message msgContent = Message.Execute();
+
+                        if (msgContent != null)
+                        {
+                            string FromAddress = string.Empty;
+                            string Date = string.Empty;
+                            string Subject = string.Empty;
+                            string MailBody = string.Empty;
+                            string ReadableText = string.Empty;
+
+                            //Loop through the headers and get fields we need (Subject, Mail)
+                            foreach (var messagePart in msgContent.Payload.Headers)
+                            {
+                                if (messagePart.Name == "From")
+                                {
+                                    FromAddress = messagePart.Value;
+                                }
+                                else if (messagePart.Name == "Date")
+                                {
+                                    Date = messagePart.Value;
+                                }
+                                else if (messagePart.Name == "Subject")
+                                {
+                                    Subject = messagePart.Value;
+                                }
+                            }
+
+                            //Read mail body
+                            Console.WriteLine("Step-2: Read Mail Body");
+                            List<string> FileName = GmailAPIHelper.GetAttachments(
+                                HostEmailAddress,
+                                msg.Id,
+                                Convert.ToString(ConfigurationManager.AppSettings["GmailAttach"]));
+
+                            if (FileName.Count() > 0)
+                            {
+                                foreach (var eachFile in FileName)
+                                {
+                                    //Get user Id using from Email Address
+                                    string[] rectifyFromAddress = FromAddress.Split(' ');
+                                    string fromAdd = rectifyFromAddress[rectifyFromAddress.Length - 1];
+
+                                    if (!String.IsNullOrEmpty(fromAdd))
+                                    {
+                                        fromAdd = fromAdd.Replace("<", string.Empty);
+                                        fromAdd = fromAdd.Replace(">", string.Empty);
+                                    }
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("Step-3: Mail has no attachments.");
+                            }
+
+                            //Read mail body
+                            MailBody = String.Empty;
+                            if (msgContent.Payload.Parts == null && msgContent.Payload.Parts != null)
+                            {
+                                MailBody = msgContent.Payload.Body.Data;
+                            }
+                            else
+                            {
+                                MailBody = GmailAPIHelper.MsgNestedParts(msgContent.Payload.Parts);
+                            }
+
+                            //BASE64 TO READABLE TEXT--------------------------------------------------------------------------------
+                            ReadableText = string.Empty;
+                            ReadableText = GmailAPIHelper.Base64Decode(MailBody);
+
+                            Console.WriteLine("STEP-4: Identifying & Configure Mails.");
+
+                            if (!string.IsNullOrEmpty(ReadableText))
+                            {
+                                Gmail GMail = new Gmail();
+                                GMail.From = FromAddress;
+                                GMail.Body = ReadableText;
+                                GMail.MailDateTime = Convert.ToDateTime(Date);
+                                emailList.Add(GMail);
+                            }
+                        }
+                    }
+                }
+                return emailList;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex);
+
+                return null;
+            }
+        }
+    }
+}
